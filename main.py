589e6195dd9ec09f70a8b6b75efbeb73a0cd0d71
Juan Bravo <113551841+jjbravo0521@users.noreply.github.com>
2022-09-20 20:06:31 +0000

    done w 1

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#group : Juan B,Diego S, Aleandro
+
 # fork this challenge to your own repl
 #share this challenge with another person in the room.
 
 #Day 3 Python Challenge
-
 # The task is as follows: You are going to create a program that first asks the user to enter text. It can be any text, an entire article, a paragraph, a sentence, a poem, whatever you want. Then the program will ask the user to enter three random letters. From that moment on, our code is going to process that information and result in five different types of analysis:
+
 
 # 1. How many times each of those letters they have chosen appears. To achieve this, I advise you to store those letters in a list, and then use a method of strings that allows you to count how many times a substring appears within the string. One thing to keep in mind is that when searching for letters, there can be upper and lower case and this will affect the result. So, to make sure that absolutely all letters are counted, you should pass both the original text and the letters to be searched to lowercase.
 
@@ -14,4 +16,12 @@
 # 4. The system will show us how the text would look like if we inverted the order of the words. Is there any method that allows us to invert the order of a list? And another one that allows us to join these elements with spaces in between?
 
 # 5. The system will tell us if the word “python” is inside the text. This part can be a bit complicated to imagine, but I'll give you a hint: you can use Booleans to make your enquiry and a dictionary to find ways to express your answer.
+
+# intro
+user_text = input("Enter a piece of text:")
+print(user_text)
+randomLetters = input("write three random letters:")
+print(randomLetters)
+print(user_text.count(randomLetters))
+
 
